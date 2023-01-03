b21fe7fc02293e5bca673647bd1be38dbaa4dcd7
PradeepVoorukonda <pradeep.voorukonda1@nhs.net>
2023-01-03 13:31:23 +0000

    update doctor details

--- CogniSmiles/Pages/Dashboard/MyAccount.cshtml.cs
+++ CogniSmiles/Pages/Dashboard/MyAccount.cshtml.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.AspNetCore.Mvc.Rendering;
+﻿using Microsoft.AspNetCore.Mvc;
 using CogniSmiles.Data;
 using CogniSmiles.Models;
 using Microsoft.EntityFrameworkCore;
