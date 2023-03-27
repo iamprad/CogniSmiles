d3e4a134a28ddf0f8c878fae082e51b54dad25c5
PradeepVoorukonda <pradeep.voorukonda1@nhs.net>
2023-03-27 18:52:22 +0100

    transfer patient and patient search box / increase file size to 200MB

--- CogniSmiles/Pages/Dashboard/Home.cshtml.cs
+++ CogniSmiles/Pages/Dashboard/Home.cshtml.cs
@@ -2,44 +2,85 @@ using CogniSmiles.Data;
 using CogniSmiles.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.FileSystemGlobbing.Internal.PatternContexts;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.InteropServices;
 
 namespace CogniSmiles.Pages.Dashboard
 {
     public class HomeModel : AuthModel
     {
-        private readonly CogniSmilesContext _context;        
-        public IList<Patient> Patient { get; set; } = default!;
+        private readonly CogniSmilesContext _context;
+        public IList<PatientData> PatientList { get; set; } = default!;
+        [BindProperty]
+       
+        public string PatientSearchTerm { get; set; }
         public HomeModel(CogniSmilesContext context)
-        {           
+        {
             _context = context;
-            Patient = new List<Patient>();
-           
+            PatientList = new List<PatientData>();
+
         }
         public async Task<ActionResult> OnGetAsync()
-        {            
+        {
             if (!IsAuthenticated)
                 return RedirectToPage("../Index");
-            
+
             else if (IsAuthenticated && DoctorId > 0)
             {
-                if(IsAdmin)
-                {
-                    Patient = await _context.Patient.ToListAsync();
-                }
-                else {
-                    Patient = await _context.Patient.Where(p => p.DoctorId == DoctorId).ToListAsync();
-                }                
+                PopulatePatientList();
             }
             return Page();
         }
-        
+        public async Task<IActionResult> OnPostAsync()
+        {            
+            PopulatePatientList();
+            return Page();
+        }
         public ActionResult OnGetSignOut()
         {
             if (!IsAuthenticated)
                 return RedirectToPage("../Index");
 
-            ClearSession();            
-            return RedirectToPage("../Index"); 
+            ClearSession();
+            return RedirectToPage("../Index");
+        }
+        private void PopulatePatientList()
+        {
+            var patientData = from patient in _context.Set<Patient>()
+                              join doctor in _context.Set<Doctor>()
+                                  on patient.DoctorId equals doctor.Id
+                              select new PatientData
+                              {
+                                  Id = patient.Id,
+                                  DoctorId = patient.DoctorId,
+                                  PracticeName = doctor.PracticeName,
+                                  PatientCode = patient.PatientCode,
+                                  SurgicalGuideReturnDate = patient.SurgicalGuideReturnDate,
+                                  ImplantSite = patient.ImplantSite,
+                                  ImplantSystem = patient.ImplantSystem,
+                                  ImplantDiameter = patient.ImplantDiameter,
+                                  ImplantLength = patient.ImplantLength,
+                                  PatientStatus = patient.PatientStatus
+                              };
+            if(!string.IsNullOrEmpty(PatientSearchTerm))
+            {
+                patientData = patientData.Where( p => p.PatientCode.Contains(PatientSearchTerm) || p.PracticeName.Contains(PatientSearchTerm) );
+            }
+            if (IsAdmin)
+            {
+                PatientList = patientData.ToList();
+            }
+            else
+            {
+                PatientList = patientData.Where(p => p.DoctorId == DoctorId).ToList();
+            }
         }
     }
+    public class PatientData : Patient
+    {
+        [Display(Name = "Practice Name")]
+        public string PracticeName;
+    }
+   
 }
---
+++ CogniSmiles/Pages/Dashboard/TransferPatient.cshtml.cs
@@ -0,0 +1,79 @@
+﻿using CogniSmiles.Data;
+using CogniSmiles.Models;
+using Microsoft.AspNetCore.Mvc;
+using CogniSmiles.Interfaces;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+
+namespace CogniSmiles.Pages.Dashboard
+{
+    public class TransferPatientModel : AuthModel
+    {
+        private readonly CogniSmilesContext _context;
+        private readonly IEmailService _emailService;
+        public TransferPatientModel(CogniSmilesContext context, IEmailService emailService)
+        {
+            _context = context;  
+            _emailService = emailService;
+        }
+        [BindProperty]
+        public int? PatientId { get; set; } = default!;
+
+        public string PracticeName { get; set; } = default!;
+        [BindProperty]
+        public string PatientCode { get; set; } = default!;
+        [BindProperty]
+        public string NewPractice { get; set; } = default!;
+        
+        public Dictionary<int,string> PracticeList { get; set; }
+
+        
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (!IsAuthenticated && (id == null || _context == null))
+            {
+                return NotFound();
+            }
+
+            var patient = _context.Patient.Where(d1 => d1.Id == id).FirstOrDefault();
+            if (patient == null || _context.Doctor == null)
+            {
+                return BadRequest();
+            }
+            PatientId = id;
+            PatientCode = patient.PatientCode!;
+#pragma warning disable CS8601 // Possible null reference assignment.
+            PracticeName = _context.Doctor.Where(d1 => d1.Id == patient.DoctorId).Select(d => d.PracticeName).FirstOrDefault();
+
+            if (PracticeName == null)
+            {
+                return BadRequest();
+            }
+            NewPractice = _context.Doctor.Where(d1 => d1.Id == DoctorId).Select(d => d.PracticeName).FirstOrDefault();
+#pragma warning restore CS8601 // Possible null reference assignment.
+
+            return Page();
+        }
+
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+            
+            var patient = _context.Patient.Where(d1 => d1.Id == PatientId).FirstOrDefault();
+            if (patient == null)
+            {
+                return BadRequest();
+            }
+            patient.DoctorId = DoctorId;
+
+            _context.Patient.Update(patient);
+            return RedirectToPage("./Home");
+        }
+
+    }
+}
