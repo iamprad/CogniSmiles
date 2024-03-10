0f447d31cd859bc4c544e7880b71e7ac63c6d5df
PradeepVoorukonda <pradeepvoorukonda@ymail.com>
2024-03-10 00:58:00 +0000

    New Course Added

---
+++ CogniSmiles/Pages/Courses/Archived/Course1.cshtml.cs
@@ -0,0 +1,23 @@
+using CogniSmiles.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Stripe;
+using Stripe.Checkout;
+
+namespace CogniSmiles.Pages.Courses
+{
+    public class Course1Model : PageModel
+    {
+
+        public Course1Model()
+        {
+                    
+        }
+
+        public void OnGet()
+        {           
+
+        }
+       
+    }
+}
